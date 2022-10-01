82fea957a4f7a7050c3d65429d29324aa3ecbd58
vgelbgras <vgelbgra@gmail.com>
2022-10-01 13:31:38 +0200

    Update test_buttons_callbacks.py

--- tests/test_buttons_callbacks.py
+++ tests/test_buttons_callbacks.py
@@ -14,7 +14,6 @@ from src.chronos.components import buttons
                              ("reset-button", 0, 1, 0, [], '00:00:00.000', '', ('START', 'success', True, 0, [], 0)),
                              ("reset-button", 1, 1, 0, [], '00:00:00.000', 'START', ('START', 'success', True, 0, [], 0)),
                              ("reset-button", 1, 1, 1, ['Dummy', 'Dummy', 'Dummy'], '00:00:00.100', 'PAUSE', ('START', 'success', True, 0, [], 0)),
-                             ("laps-button", 1, 0, 1, [], '00:00:00.100', 'PAUSE', (no_update, no_update, no_update, no_update, [], no_update))
                              ])
 def test_update_callback(button_id, n_clicks_start, n_clicks_reset, n_click_lap, table_state, timer_state, start_button_state, expected):
     context_value.set(AttributeDict(**{"triggered_inputs": [{"prop_id": f"{button_id}.n_clicks"}]}))
@@ -22,15 +21,20 @@ def test_update_callback(button_id, n_clicks_start, n_clicks_reset, n_click_lap,
     assert output == expected
 
 
-def test_update_callback_for_lap_click():
+@pytest.mark.parametrize("n_clicks_lap, table_state, timer_state, expected_element_in_updated_table, expected_row",
+                         [
+                             (1, [], '00:00:00.700', 2, """Tbody([Tr([Td('1'), Td('00:00:00.700')])])"""),
+                             (2, ["""Thead(Tr([Th('Lap #'), Th('Time')]))""", """Tbody([Tr([Td('1'), Td('00:00:00.800')])])"""], '00:00:00.700', 3, """Tbody([Tr([Td('2'), Td('00:00:00.800')])])"""),
+                            ])
+def test_update_callback_for_lap_click(n_clicks_lap, table_state, timer_state, expected_element_in_updated_table, expected_row):
     button_id = "laps-button"
     context_value.set(AttributeDict(**{"triggered_inputs": [{"prop_id": f"{button_id}.n_clicks"}]}))
-    (output_1, output_2, output_3, output_4, output_5, output_6) = buttons.update(n_clicks_start=1, n_clicks_reset=0, n_clicks_lap=2, start_button_state='PAUSE', table_state=[], timer_state='00:00:00.000')
+    (output_1, output_2, output_3, output_4, output_5, output_6) = buttons.update(n_clicks_start=1, n_clicks_reset=0, n_clicks_lap=n_clicks_lap, start_button_state='PAUSE', table_state=table_state, timer_state=timer_state)
     assert output_1 == no_update
     assert output_2 == no_update
     assert output_3 == no_update
     assert output_4 == no_update
-    assert len(output_5) == 2
+    assert len(output_5) == expected_element_in_updated_table
     output_5[0] = """Thead(Tr([Th('Lap #'), Th('Time')]))"""
-    output_5[1] = """Tbody([Tr([Td('2'), Td('00:00:00.000')])])"""
+    output_5[-1] = expected_row
     assert output_6 == no_update
